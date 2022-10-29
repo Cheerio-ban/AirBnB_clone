f711ff183b94c8a72c74b4505ab430487facd407
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-10-29 06:08:09 +0100

    Base model and storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,28 @@
 """
 
 from datetime import datetime
+import models
 import uuid
 
 
 class BaseModel:
     """Base model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the BaseModel Class."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for elem, value in kwargs.items():
+                if elem == __class__:
+                    continue
+                if elem == 'created_at' or elem == 'updated_at':
+                    self.__dict__[elem] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[elem] = value
+        else:
+            models.storage.new(self)
+
     
     def __str__(self):
         """Returns a string representation of the base class"""
@@ -23,6 +35,7 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns the dictionary of the base model class"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    Contains class that serializes instances to a json
+    file and deserializes json file to instances
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the private class instance dictionary objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets obj parameter name.id as the attribute name to the obj object"""
+        FileStorage.__objects["{}.{}".format(obj.to_dict()["__class__"], obj.id)] = obj
+
+    def save(self):
+        """Serializes private instance __objects to private instance file_path"""
+        save_obj = {}
+        for key, value in FileStorage.__objects.items():
+            save_obj[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(save_obj, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                objdict = json.load(f)
+                for elem in objdict.values():
+                    cls_name = elem["__class__"]
+                    self.new(eval(cls_name)(**elem))
+            
+        except FileNotFoundError or FileExistsError:
+            return
+
