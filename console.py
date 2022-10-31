d9b91e551ce7f2a087fe7fd71337003fb5504ca9
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-10-31 03:20:02 +0100

    Updated the console

--- console.py
+++ console.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Airbnb console program."""
-
+"""Airbnb console module."""
 import cmd
 from models.base_model import BaseModel
 from models.user import User
 import models
+import json
+import shlex
 from models.place import Place
 from models.city import City
 from models.review import Review
@@ -14,7 +15,9 @@ from models.amenity import Amenity
 
 class HBNBCommand(cmd.Cmd):
     """Represents the console interpreter"""
-    prompt = "(hbnb)"
+
+    prompt = "(hbnb) "
+    file = None
     __classes = {
         "BaseModel": BaseModel(),
         "User": User(),
@@ -24,6 +27,50 @@ class HBNBCommand(cmd.Cmd):
         "State": State(),
         "Amenity": Amenity(),
     }
+
+    def parse_input(self, line: str):
+        """Parses the command line arguments"""
+        if '"' in line:
+            inp = shlex.split(line)
+            inp = [o.strip('"') for o in inp]
+            return inp
+        inp = line.split(" ")
+        return inp
+
+    def default(self, line):
+        """This overrides the default command"""
+        cmds = {"all()": self.do_all,
+                "count()": self.count
+                }
+        c2 = {"show": self.do_show,
+              "destroy": self.do_destroy,
+              "update": self.do_update
+              }
+        ar = line.split(".")
+        if "." not in line:
+            print("** Unknown syntax: {} **".format(line))
+            return
+        if ar[1] not in cmds.keys() and ar[1].split("(")[0] not in c2.keys():
+            print("** Unknown syntax: {} **".format(line))
+            return
+        if ar[1] in cmds:
+            cmds[ar[1]]("{}".format(ar[0]))
+        else:
+            if "update" not in ar[1]:
+                id = ar[1].split('(')[1].split(')')[0]
+                c2[ar[1].split("(")[0]]("{} {}".format(ar[0], id))
+            else:
+                if "{" not in ar[1]:
+                    id = ar[1].split("(")[1].split(",")[0]
+                    atr = ar[1].split("(")[1].split(",")[1]
+                    al = ar[1].split("(")[1].split(",")[2].split(")")[0]
+                    cmd = ar[1].split("(")[0]
+                    c2[cmd]('{} {} {} "{}" '.format(ar[0], id, atr, al))
+                else:
+                    id = ar[1].split("(")[1].split("{")[0].strip(", ")
+                    at_d = '{' + ar[1].split("(")[1].split("{")[1].strip(")")
+                    cmd = ar[1].split("(")[0]
+                    c2[cmd]('{}.{} {}'.format(ar[0], id, at_d))
 
     def do_EOF(self, line):
         """End of file command to exit the program"""
@@ -52,7 +99,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, line):
         """usage: show BaseModel 1234-1234-1234"""
-        lines = line.split()
+        lines = self.parse_input(line)
         if len(line) == 0:
             print("** class name missing **")
             return
@@ -69,8 +116,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """usage: destroy BaseModel 1234-1234-1234"""
-        lines = line.split()
-        if len(lines) == 0:
+        lines = self.parse_input(line)
+        if len(line) == 0:
             print("** class name missing **")
             return
         if lines[0] and lines[0] not in HBNBCommand.__classes.keys():
@@ -88,7 +135,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, line):
         """usage: all <class name>"""
-        lines = line.split()
+        lines = self.parse_input(line)
         if lines[0] and lines[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
@@ -106,8 +153,28 @@ class HBNBCommand(cmd.Cmd):
             print(new_arr)
 
     def do_update(self, line):
-        """usage:update <class name> <id> <attribute name> <attribute value>"""
-        lines = line.split()
+        """
+        usage: update <class name> <id> <attribute name>
+        '<attribute value>'
+        """
+        if "{" in line:
+            ar = line.split(".")
+            cls = ar[0]
+            id_no = ar[1].split("{")[0].strip(" ")
+            at_d = ar[1].split("{")[-1].strip("}")
+            at_d = at_d.split(",")
+            b = {o.split(":")[0].strip(" '").strip(' "'):
+                    o.split(":")[1].strip(" '").strip(' "') for o in at_d}
+            objs = models.storage.all()
+            if not any(obj.id == id_no for obj in objs.values()
+                        if obj.__class__.__name__ == cls):
+                print("** no instance found **")
+            else:
+                obj = objs["{}.{}".format(ar[0], id_no)]
+                [setattr(obj, k, v) for k, v in b.items()]
+            models.storage.save()
+            return
+        lines = self.parse_input(line)
         if len(lines) == 0:
             print("** class name missing **")
             return
@@ -139,6 +206,31 @@ class HBNBCommand(cmd.Cmd):
             lines[3] = type(obj.__class__.__dict__[lines[2]])(lines[3])
             obj.__dict__[lines[2]] = lines[3]
         models.storage.save()
+
+    def count(self, line):
+        """
+            Count the total instances of.
+            a supported model from our file storage.
+        """
+        cnt = 0
+        line = self.parse_input(line).replace('count', '').replace(' ', '')
+        if line not in HBNBCommand.__classes.keys():
+            print("** class doesn't exist **")
+            return
+        try:
+            with open('file.json', 'r', encoding='utf-8') as f:
+                objs = json.loads(f.read())
+            for obj in objs.values():
+                name = obj['__class__']
+                if name == line:
+                    cnt += 1
+            print(cnt)
+        except Exception:
+            pass
+
+    def to_dict(self, obj):
+        """Convert @obj to  dict"""
+        return eval(obj)
 
 
 if __name__ == '__main__':
